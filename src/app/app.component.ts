0dc095bd6393f885570ee9c2c26037135b1169c8
Phil Gibbins <scoobster17@hotmail.co.uk>
2017-08-23 16:50:15 +0100

    Added strong typing to main app values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hawk Timer';
-  description = 'Multi-split timer so you can track your time, whether it be for your your own use, for your timesheets, interest, or if someone is watching you like a hawk!';
-  callToActions = {
+  title: string = 'Hawk Timer';
+  description: string = 'Multi-split timer so you can track your time, whether it be for your your own use, for your timesheets, interest, or if someone is watching you like a hawk!';
+  callToActions: object = {
     startTimer: 'Start a timer'
   }
 }
